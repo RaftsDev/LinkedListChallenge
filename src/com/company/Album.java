5b9e88fe058f9a63427dd5144a82e365f738738d
Ralph F <RaftsDev@gmail.com>
2020-09-12 11:26:36 -0400

    Feat: Add task to Main class. Create Album class.

--- src/com/company/Album.java
+++ src/com/company/Album.java
@@ -1,2 +1,21 @@
-package com.company;public class Album {
+package com.company;
+
+import java.util.ArrayList;
+
+public class Album {
+    private String name;
+    private ArrayList<Song> songs;
+
+    public Album(String name) {
+        this.name = name;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
 }
